f07aea486698e269d844e3f2b5391b46d063b4d4
Chadrack Kanza <chadrackkanza2015@gmail.com>
2023-07-08 19:17:07 +0100

    Lesson 2 : Import et Export des composants

---
+++ src/Pages/Home.js
@@ -0,0 +1,16 @@
+export const Navbar = () => {
+    return(
+        <h1>
+            My navbar
+        </h1>
+    )
+}
+
+export default function Home(){
+    return(
+        <>
+            <h1>Page Home</h1>
+        </>
+    )
+}
+    
